4175f2e92157b46ff77bc26a65ee6601f4e39c10
Nihalkhamis <nihalyasser23@gmail.com>
2022-05-15 07:07:14 -0700

    folders of LeaguesDetails added

---
+++ SportsApplication/Module/LeaguesDetails/Presenter/LeaguesDetailsPresenterProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  LeaguesDetailsPresenterProtocol.swift
+//  SportsApplication
+//
+//  Created by nihal yasser khamis on 5/15/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import Foundation
